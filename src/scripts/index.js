486cae753609e260a1291cf36a17d122e27eba3b
thufailadib <Thufails505@gmail.com>
2021-05-19 09:29:09 +0700

    Apply Suggestion :D

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,8 +3,8 @@ import '../styles/main.css';
 import '../styles/responsive.css';
 import data from '../DATA.json'
 
-const menu = document.querySelector('#menu');
-const hero = document.querySelector('.hero');
+const menu = document.querySelector("#menu");
+const hero = document.querySelector(".hero");
 const main = document.querySelector("main");
 const drawer = document.querySelector("#drawer");
 
